02cc4dd65375265e0773ee054d50318cc8eaccc3
VinithReddy <vinithr724@gmail.com>
2024-02-29 15:39:16 -0600

    chngsssss

--- src/main/java/com/CISProject/LibraryService/model/Book.java
+++ src/main/java/com/CISProject/LibraryService/model/Book.java
@@ -11,41 +11,4 @@ public class Book {
     private int yearPublished;
     private int quantity;
     private List<BookIsbnDTO> bookIsbns;
-
-    /*private Integer bookId;
-    private String author;
-    private String publisher;
-    private int yearOfPublish;
-
-    public void setBookId(Integer bookId) {
-        this.bookId = bookId;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
-    public void setYearOfPublish(Integer yearOfPublish) {
-        this.yearOfPublish = yearOfPublish;
-    }
-
-    public Integer getBookId() {
-        return bookId;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public Integer getYearOfPublish() {
-        return yearOfPublish;
-    }*/
 }
